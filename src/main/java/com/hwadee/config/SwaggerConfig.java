21a1985f66a08016f24a7a734dcb6b5244c6520c
PixelPilot666 <daniel2517liu@gmail.com>
2023-07-29 10:41:57 +0800

    update SwaggerConfig.java

---
+++ src/main/java/com/hwadee/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.hwadee.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder().title("虚拟仿真实训管理云平台API文档")
+                .description("虚拟仿真实训管理云平台API文档")
+                .version("V1.0")
+                .build();
+    }
+
+    @Bean
+    public Docket productApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
