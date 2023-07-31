98886b42f324fad576c115b72919eaf9274fc8cb
hdh25490 <13896922098@163.com>
2023-07-31 15:36:40 +0800

    update StudentMapper.java

--- src/main/java/com/hwadee/mapper/StudentMapper.java
+++ src/main/java/com/hwadee/mapper/StudentMapper.java
@@ -2,9 +2,11 @@ package com.hwadee.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hwadee.entity.Student;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  *  学生Mapper接口
  */
+@Mapper
 public interface StudentMapper extends BaseMapper<Student> {
 }
